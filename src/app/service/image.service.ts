6b0ef992806e64854c44dc3e5594d44c7d5d1f72
janetca30 <janetca30@gmail.com>
2023-03-25 14:54:55 -0300

    arreglos

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
 import { Storage, getDownloadURL, list, ref, uploadBytes} from '@angular/fire/storage'
 
 @Injectable({
@@ -29,4 +29,35 @@ export class ImageService {
     })    
     .catch(error => console.log(error))      
   }
-}
+}*/
+
+import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  url: string = "";
+
+  constructor(private storage: AngularFireStorage) { }
+
+  public uploadImage($event: any, name: string){
+    const file = $event.target.files[0];
+    const imgRef = this.storage.ref(`imagen/${name}`);
+    this.storage.upload(`imagen/${name}`, file)
+    .catch(error => console.log(error));
+  }
+
+  getImages(path: string){
+    const imagesRef = this.storage.ref(path);
+    imagesRef.listAll()
+    .then(async response => {
+      for(let item of response.items){
+        this.url = await item.getDownloadURL();
+        console.log("La URL es: " + this.url);
+      }
+    })    
+    .catch(error => console.log
+
+
