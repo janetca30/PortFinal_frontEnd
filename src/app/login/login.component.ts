709dfae3cc48d793b35f230d9ebf0c62b18599fd
janetca30 <janetca30@gmail.com>
2023-03-26 23:29:11 -0300

    cambiando login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,7 +42,10 @@ export class LoginComponent implements OnInit{
       this.isLogged = false;
       this.isLogginFail = true;
       this.errMsj = err.error.mensaje;
-      console.log(this.errMsj);
-    })
+      console.log(this.loginUsuario);
+    }
+    )
   } 
+  
+  
 }
